37878f7f2f27a2e5a69d2bd971a8b92c1478b0cf
Wenbin Yang <bysin7@gmail.com>
2021-10-14 18:11:59 +0200

    Update subBgStream.py

--- subBgStream.py
+++ subBgStream.py
@@ -116,6 +116,8 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                         for (x, y, w, h) in rects:
                             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 20)
                         
+                        ## thresholding
+                        ret, frame = cv2.threshold(frame, 80, 255, cv2.THRESH_BINARY)
 
 
                         ### and now we convert it back to JPEG to stream it
